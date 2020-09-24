59c86b2a799ffed541295a7dab553afb09298d0f
Overblade <overblade.git@gmail.com>
2020-09-24 19:04:29 +0200

    Added arc_assert

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include "util/log.h"
 #include "util/vector.h"
 #include "util/format.h"
+#include "util/assert.h"
 
 
 int main(){
@@ -12,6 +13,9 @@ int main(){
 	Vec2 a(1.2, 3.9);
 	Vec2 b(0.3, 1.11);
 	double x = Math::toDegrees(a.angle(b));
+
+	arc_assert(1 < -32, "dd%s %f {appname}", "kkkkkek", 3);
+	
 
 	Log::debug("Core", "{appname}: The angle between the two vectors equals %f", x);
 	
---
+++ src/util/assert.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "util/log.h"
+
+
+template<class... Args>
+void __arc_assert(bool condition, const std::string& file, int line, const std::string& message, const Args&... args) {
+
+	if (!condition) {
+		Log::error("Assert", "Assertion failed (File " + file + ", line " + std::to_string(line) + "): " + message, args...);
+	}
+
+}
+
+
+#ifndef NDEBUG
+	#define arc_assert(cond, msg, ...)	do {__arc_assert((cond), __FILE__, __LINE__, (msg), ##__VA_ARGS__);} while (false)
+#else
+	#define arc_assert(cond, msg, ...)
+#endif
