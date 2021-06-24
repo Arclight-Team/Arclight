7b72b53e9f108e62359fd3de10dadc2eac8b7571
Ed_IT <wmusicwtf@gmail.com>
2021-06-24 16:17:17 +0200

    Made Util::format noexcept

--- src/util/format.h
+++ src/util/format.h
@@ -8,40 +8,40 @@
 namespace Util {
 
 
-    template<class... Args>
-    std::string format(std::string message, Args&&... args) noexcept {
+	template<class... Args>
+	std::string format(std::string message, Args&&... args) noexcept {
 
-        std::string out;
-        out.resize(message.length() * 2);
+		try {
+			std::string out;
+			out.resize(message.length() * 2);
 
-        std::snprintf(out.data(), out.size(), message.c_str(), args...);
+			std::snprintf(out.data(), out.size(), message.c_str(), args...);
 
-        try {
-            return out;
-        }
-        catch (std::exception&) {
-            //We cannot log here, so return an empty string
-            return "";
-        }
+			return out;
+		}
+		catch (std::exception&) {
+			//We cannot log here, so return an empty string
+			return "";
+		}
 
-    }
+	}
 
-    template<class... Args>
-    std::wstring format(std::wstring message, Args&&... args) noexcept {
+	template<class... Args>
+	std::wstring format(std::wstring message, Args&&... args) noexcept {
 
-        std::wstring out;
-        out.resize(message.length() * 2);
+		try {
+			std::wstring out;
+			out.resize(message.length() * 2);
 
-        std::swprintf(out.data(), out.size(), message.c_str(), args...);
+			std::swprintf(out.data(), out.size(), message.c_str(), args...);
 
-        try {
-            return out;
-        }
-        catch (std::exception&) {
-            //We cannot log here, so return an empty string
-            return "";
-        }
+			return out;
+		}
+		catch (std::exception&) {
+			//We cannot log here, so return an empty string
+			return "";
+		}
 
-    }
+	}
 
 }
