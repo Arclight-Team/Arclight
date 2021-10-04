a58082848f62032cc1ed3ebbf94e2fd7d97dbb1a
Arcayn <arcayn.arclight@gmail.com>
2021-10-05 01:08:47 +0200

    Finished CodeConv

--- src/util/codeconv.h
+++ src/util/codeconv.h
@@ -92,26 +92,46 @@ namespace CodeConv {
     //UTF to codepoint
     template<Encoding E>
     constexpr static u32 toCodepoint(const std::string_view& in) {
+        u32 dummy;
+        return toCodepoint<E>(in, dummy);
+    }
+
+    template<Encoding E>
+    constexpr static u32 toCodepoint(const std::string_view& in, u32& readBytes) {
+
+        constexpr static u32 errorCodepoint = 0xFFFD;
 
         if constexpr (E == Encoding::UTF8) {
+
+            if(in.size() < 1) { readBytes = 0; return errorCodepoint; }
 
             u8 c0 = in[0];
 
             if(c0 < 0x80) {
 
+                readBytes = 1;
                 return c0;
 
             } else if (c0 < 0xE0) {
 
+                if(in.size() < 2) { readBytes = 0; return errorCodepoint; }
+
+                readBytes = 2;
                 return (c0 & 0x1F) << 6 | in[1] & 0x3F;
 
             } else if (c0 < 0xF0) {
+                
+                if(in.size() < 3) { readBytes = 0; return errorCodepoint; }
 
+                readBytes = 3;
                 return (c0 & 0x0F) << 12 | (in[1] & 0x3F) << 6 | in[2] & 0x3F;
 
             } else {
 
+                if(in.size() < 4) { readBytes = 0; return errorCodepoint; }
+
                 //Assume we have a valid UTF-8 high byte
+                readBytes = 4;
                 return (c0 & 0x07) << 18 | (in[1] & 0x3F) << 12 | (in[2] & 0x3F) << 6 | in[3] & 0x3F;
 
             }
@@ -119,6 +139,8 @@ namespace CodeConv {
         } else if constexpr (E == Encoding::UTF16LE || E == Encoding::UTF16BE) {
 
             constexpr static bool convert = Bits::requiresEndianConversion(E == Encoding::UTF16LE ? ByteOrder::Little : ByteOrder::Big);
+
+            if(in.size() < 2) { readBytes = 0; return errorCodepoint; }
 
             u16 c0 = in[1] << 8 | in[0];
 
@@ -128,6 +150,8 @@ namespace CodeConv {
 
             if (c0 >= 0xD800 && c0 < 0xDC00) {
 
+                if(in.size() < 4) { readBytes = 0; return errorCodepoint; }
+
                 u16 c1 = in[3] << 8 | in[2];
 
                 if constexpr (convert) {
@@ -135,10 +159,12 @@ namespace CodeConv {
                 }
 
                 //Assume we have a low-surrogate in c1
+                readBytes = 4;
                 return (c0 & 0x3FF) << 10 | (c1 & 0x3FF);
 
             } else {
 
+                readBytes = 2;
                 return c0;
 
             }
@@ -147,12 +173,15 @@ namespace CodeConv {
 
             constexpr static bool convert = Bits::requiresEndianConversion(E == Encoding::UTF32LE ? ByteOrder::Little : ByteOrder::Big);
 
+            if(in.size() < 4) { readBytes = 0; return errorCodepoint; }
+
             u32 codepoint = in[3] << 24 | in[2] << 16 | in[1] << 8 | in[0];
 
             if constexpr (convert) {
                 codepoint = Bits::swap32(codepoint);
             }
 
+            readBytes = 4;
             return codepoint;
 
         }
@@ -160,9 +189,38 @@ namespace CodeConv {
     }
 
 
+    //UTF inter-conversion
+    template<Encoding From, Encoding To>
+    constexpr static u32 convertUnicode(const std::string_view& in) {
+
+        std::string convertedString;
+        convertedString.reserve(in.size());
+
+        u32 i = 0;
+
+        while(i < in.size()) {
+
+            u32 parsedBytes = 0;
+            u32 codepoint = toCodepoint<From>({&in[i], in.size() - i - 1}, parsedBytes);
+
+            if(!parsedBytes) {
+                //In case of error, skip byte and hope for the best
+                parsedBytes++;
+            }
+
+            convertedString += toUTF<To>(codepoint);
+            i += parsedBytes;
+
+        }
+
+        return convertedString;
+
+    }
+
+
     //Standard Shift-JIS to Unicode
     template<Encoding E>
-    std::string shiftJISToUnicode(const std::string_view& in) {
+    std::string fromShiftJIS(const std::string_view& in) {
 
         std::string unicodeString;
         unicodeString.reserve(in.size() * 2);
@@ -210,7 +268,7 @@ namespace CodeConv {
 
     //Standard Big5 to Unicode
     template<Encoding E>
-    std::string big5ToUnicode(const std::string_view& in) {
+    std::string fromBig5(const std::string_view& in) {
 
         std::string unicodeString;
         unicodeString.reserve(in.size() * 2);
@@ -244,7 +302,7 @@ namespace CodeConv {
 
     //Wansung with UHC extensions to Unicode
     template<Encoding E>
-    std::string wansungToUnicode(const std::string_view& in) {
+    std::string fromWansung(const std::string_view& in) {
 
         std::string unicodeString;
         unicodeString.reserve(in.size() * 2);
@@ -278,7 +336,7 @@ namespace CodeConv {
 
     //Johab to Unicode
     template<Encoding E>
-    std::string johabToUnicode(const std::string_view& in) {
+    std::string fromJohab(const std::string_view& in) {
 
         std::string unicodeString;
         unicodeString.reserve(in.size() * 2);
