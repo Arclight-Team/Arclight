d4d2aaf69294c5c461e25ed6a709f098e2b0ac54
Arcayn <arcayn.arclight@gmail.com>
2022-01-03 15:39:08 +0100

    Fixed CMake debug/release definitions, improved arc_assert

--- src/arclight/core/util/assert.hpp
+++ src/arclight/core/util/assert.hpp
@@ -9,27 +9,42 @@
 #pragma once
 
 #include "log.hpp"
+#include "types.hpp"
 #include "arcconfig.hpp"
+
+#include <source_location>
 
 #define arc_abort()						do { std::abort(); } while (false)
 #define arc_exit(x)						do { std::exit((x)); } while(false)
 
 
 template<class... Args>
-constexpr void __arc_assert(bool condition, const std::string& file, int line, const std::string& message, const Args&... args) noexcept {
+constexpr void __arc_assert(bool condition, const std::source_location& srcLocation, const std::string& message, Args&&... args) noexcept {
 
 	if (!condition) {
-		Log::error("Assert", "Assertion failed (File " + file + ", line " + std::to_string(line) + "): " + message, args...);
+
+		try {
+
+			Log::error("Assert", "Assertion failed in file '" + std::string(srcLocation.file_name()) + "' [" + std::to_string(srcLocation.line()) + ":" + std::to_string(srcLocation.column()) + "] in function '" + std::string(srcLocation.function_name()) + "':");
+			Log::error("Assert", "\t" + message, std::forward<Args>(args)...);
+
+		} catch(...) {
+
+			Log::error("Assert", "Fatal error: Failed to assert");
+
+		}
+
 		arc_abort();
+
 	}
 
 }
 
 
-#if defined(ARC_FINAL_BUILD) || (defined(ARC_ASSERT_DEBUG) && defined(NDEBUG)) || defined(ARC_ASSERT_NEVER)
+#if defined(ARC_FINAL_BUILD) || (defined(ARC_ASSERT_DEBUG) && ARC_RELEASE) || defined(ARC_ASSERT_NEVER)
 	#define arc_assert(cond, msg, ...)
 	#define arc_force_assert(msg, ...)
 #else
-	#define arc_assert(cond, msg, ...)	do {__arc_assert(!!(cond), __FILE__, __LINE__, (msg), ##__VA_ARGS__);} while (false)
-	#define arc_force_assert(msg, ...)	do {__arc_assert(false, __FILE__, __LINE__, (msg), ##__VA_ARGS__);} while (false)
+	#define arc_assert(cond, msg, ...)	do {__arc_assert(!!(cond), std::source_location::current(), (msg), ##__VA_ARGS__);} while (false)
+	#define arc_force_assert(msg, ...)	arc_assert(false, msg, __VA_ARGS__)
 #endif
