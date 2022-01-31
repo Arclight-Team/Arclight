e725f1fd32d2a6125d2002f5c5b484835f7f66de
Ed_IT <wmusicwtf@gmail.com>
2022-01-31 23:56:39 +0100

    ASX - Arclight FMOD wrapper

---
+++ src/arclight/core/sound/asx/dsp.hpp
@@ -0,0 +1,270 @@
+/*
+ *	 Copyright (c) 2021 - Arclight Team
+ *
+ *	 This file is part of Arclight. All rights reserved.
+ *
+ *	 dsp.hpp
+ */
+
+#pragma once
+#include "common.hpp"
+#include "exception.hpp"
+#include "system.hpp"
+
+#include <string>
+#include <span>
+
+
+// ASX - Arclight FMOD wrapper
+namespace ASX
+{
+	// Enums
+
+	enum class DSPConnectionType
+	{
+		Standard,
+		Sidechain,
+		Send,
+		SendSidechain
+	};
+
+	enum class DSPType
+	{
+		Unknown,
+		Mixer,
+		Oscillator,
+		Lowpass,
+		ITLowpass,
+		Highpass,
+		Echo,
+		Fader,
+		Flange,
+		Distortion,
+		Normalize,
+		Limiter,
+		ParamEQ,
+		PitchShift,
+		Chorus,
+		VSTPlugin,
+		WinampPlugin,
+		ITEcho,
+		Compressor,
+		SFXReverb,
+		LowpassSimple,
+		Delay,
+		Tremolo,
+		LADSPAPlugin,
+		Send,
+		Return,
+		HighpassSimple,
+		Pan,
+		ThreeEQ,
+		FFT,
+		LoudnessMeter,
+		EnvelopeFollower,
+		ConvolutionReverb,
+		ChannelMix,
+		Transceiver,
+		ObjectPan,
+		MultibandEQ,
+	};
+
+	// Structures
+
+	struct DSPChannelFormat
+	{
+		ChannelMask channelMask;
+		SpeakerMode speakerMode;
+		u32 channelCount;
+	};
+
+	// TODO: check correct default values
+	struct DSPWetDryMix
+	{
+		float preWet	= 0.0f;
+		float postWet	= 0.0f;
+		float dry		= 0.0f;
+	};
+
+	struct DSPDescription : private FMOD_DSP_DESCRIPTION
+	{
+
+		void setName(const std::string& name) {
+			std::strncpy(this->name, name.c_str(), 32);
+		}
+
+		std::string getName() {
+			return name;
+		}
+
+	};
+
+#define DEF_DSP_PARAMS(name, ...) struct name { enum : u32 { __VA_ARGS__ }; }
+
+	// FMOD_DSP_CONVOLUTION_REVERB
+	DEF_DSP_PARAMS(DSPConvolutionReverb, IR, Wet, Dry, Linked);
+
+	// FMOD_DSP_ECHO
+	DEF_DSP_PARAMS(DSPEcho, Delay, Feedback, Dry, Wet);
+
+#undef DEF_DSP_PARAMS
+
+	namespace Detail
+	{
+		template<DSPType Type>
+		struct SelectDSPParameter
+		{
+			using ParamType = TT::NthPackType < SizeT(Type),
+				void,					// Unknown
+				void,					// Mixer
+				void,					// Oscillator
+				void,					// Lowpass
+				void,					// ITLowpass
+				void,					// Highpass
+				DSPEcho,				// Echo
+				void,					// Fader
+				void,					// Flange
+				void,					// Distortion
+				void,					// Normalize
+				void,					// Limiter
+				void,					// ParamEQ
+				void,					// PitchShift
+				void,					// Chorus
+				void,					// VSTPlugin
+				void,					// WinampPlugin
+				void,					// ITEcho
+				void,					// Compressor
+				void,					// SFXReverb
+				void,					// LowpassSimple
+				void,					// Delay
+				void,					// Tremolo
+				void,					// LADSPAPlugin
+				void,					// Send
+				void,					// Return
+				void,					// HighpassSimple
+				void,					// Pan
+				void,					// ThreeEQ
+				void,					// FFT
+				void,					// LoudnessMeter
+				void,					// EnvelopeFollower
+				DSPConvolutionReverb,	// ConvolutionReverb
+				void,					// ChannelMix
+				void,					// Transceiver
+				void,					// ObjectPan
+				void					// MultibandEQ
+			> ;
+		};
+
+	}
+
+	template<DSPType Type>
+	using DSPParameters = Detail::SelectDSPParameter<Type>::ParamType;
+	
+	class DSP : public IHandleOwner<FMOD::DSP>, public ISystemUser
+	{
+	public:
+
+		void create(DSPType type);
+		void create(DSPDescription& description);
+		void create(u32 pluginHandle);
+
+		void destroy();
+
+		/*
+		*	Connection operations 
+		*/
+
+		void addInput(DSP& input, DSPConnection& connection, DSPConnectionType type = DSPConnectionType::Standard);
+		void addInput(DSP& input, DSPConnectionType type = DSPConnectionType::Standard);
+		
+		void disconnectFrom(DSP& target, DSPConnection& connection);
+		void disconnectFrom(DSP& target);
+
+		u32 getInputCount() const;
+		u32 getOutputCount() const;
+
+		void getInput(u32 index, DSP& input, DSPConnection& connection);
+		void getInput(u32 index, DSP& input);
+
+		void getOutput(u32 index, DSP& output, DSPConnection& connection);
+		void getOutput(u32 index, DSP& output);
+
+		/*
+		*	DSP unit control
+		*/
+
+		void setActive(bool active);
+		bool isActive() const;
+
+		inline void activate() {
+			setActive(true);
+		}
+
+		inline void deactivate() {
+			setActive(false);
+		}
+
+		void setBypass(bool bypass);
+		bool isBypassed() const;
+
+		void setWetDryMix(const DSPWetDryMix& mix);
+		DSPWetDryMix getWetDryMix() const;
+
+		void setChannelFormat(const DSPChannelFormat& format);
+		DSPChannelFormat getChannelFormat() const;
+		DSPChannelFormat getOutputChannelFormat(const DSPChannelFormat& inputFormat) const;
+
+		/*
+		*	DSP parameter control
+		*/
+
+		void setParameterFloat(u32 index, float value);
+		void setParameterInt(u32 index, int value);
+		void setParameterBool(u32 index, bool value);
+
+		template<class T>
+		void setParameterData(u32 index, std::span<T> buffer) {
+			ASX_TRY() = handle->setParameterData(index, buffer.data(), buffer.size_bytes());
+		}
+
+		void showConfigDialog(void* hwnd, bool show);
+
+		/*
+		*	Getters 
+		*/
+
+	private:
+
+		template<class T>
+		friend class IChannelControl;
+		friend class DSPConnection;
+
+	};
+
+	class DSPConnection : public IHandleOwner<FMOD::DSPConnection>
+	{
+	public:
+
+		void getInput(DSP& dsp);
+		void getOutput(DSP& dsp);
+
+		void setMix(float volume);
+		float getMix() const;
+
+		void setMixMatrix(float* matrix, int outChannels, int inChannels, int inChannelHop = 0);
+		float* getMixMatrix(int& outChannels, int& inChannels);
+
+		template<int OutChannels, int InChannels>
+		void setMixMatrix(float matrix[OutChannels][InChannels]) {
+			setMixMatrix(matrix, OutChannels, InChannels);
+		}
+
+		DSPConnectionType getType() const;
+
+	private:
+
+		friend class DSP;
+
+	};
+
+}
---
+++ src/arclight/core/sound/asx/sound.cpp
@@ -0,0 +1,347 @@
+/*
+ *	 Copyright (c) 2021 - Arclight Team
+ *
+ *	 This file is part of Arclight. All rights reserved.
+ *
+ *	 sound.cpp
+ */
+
+#include "sound.hpp"
+
+
+// ASX - Arclight FMOD wrapper
+namespace ASX
+{
+	
+	void Sound::create(const Uri& uri, const SoundOptions& opt) {
+
+		// Already constructed
+		if (handle) {
+			return;
+		}
+
+		if (opt.nonBlocking)
+			nonBlocking = true;
+
+		handle = nullptr;
+		ASX_TRY() = getSystemHandle()->createSound(uri.getPath().c_str(), opt.getFlag(), nullptr, &handle);
+
+	}
+
+	void Sound::create(const Uri& uri, CreateSoundInfo& info, const SoundOptions& opt) {
+
+		// Already constructed
+		if (handle) {
+			return;
+		}
+
+		if (opt.nonBlocking)
+			nonBlocking = true;
+
+		handle = nullptr;
+		ASX_TRY() = getSystemHandle()->createSound(uri.getPath().c_str(), opt.getFlag(), &info, &handle);
+
+	}
+
+	void Sound::destroy() {
+
+		if (handle) {
+			ASX_TRY() = handle->release();
+			handle = nullptr;
+		}
+
+	}
+
+	void Sound::play(Channel& channel, bool paused) {
+
+		FMOD::Channel* fmodHandle{};
+
+		playImpl(paused, nullptr, &fmodHandle);
+
+		// Acquire new handle and release old one
+		channel.acquire(fmodHandle);
+
+	}
+
+	void Sound::play(Channel& channel, const SoundOptions& opt, bool paused) {
+
+		FMOD::Channel* fmodHandle{};
+
+		playImpl(opt, paused, nullptr, &fmodHandle);
+
+		// Acquire new handle and release old one
+		channel.acquire(fmodHandle);
+
+	}
+
+	void Sound::play(ChannelGroup& group, Channel& channel, bool paused) {
+
+		FMOD::Channel* fmodHandle{};
+
+		playImpl(paused, group.handle, &fmodHandle);
+
+		// Acquire new handle and release old one
+		channel.acquire(fmodHandle);
+
+	}
+
+	void Sound::play(ChannelGroup& group, Channel& channel, const SoundOptions& opt, bool paused) {
+
+		FMOD::Channel* fmodHandle{};
+
+		playImpl(opt, paused, group.handle, &fmodHandle);
+
+		// Acquire new handle and release old one
+		channel.acquire(fmodHandle);
+
+	}
+
+	void Sound::setDefaults(const SoundDefaults& defaults) {
+		ASX_TRY() = handle->setDefaults(defaults.frequency, defaults.priority);
+	}
+
+	SoundDefaults Sound::getDefaults() const {
+
+		float frequency{};
+		i32 priority{};
+
+		ASX_TRY() = handle->getDefaults(&frequency, &priority);
+
+		return { frequency, priority };
+
+	}
+
+	void Sound::set3DMinMaxDistance(const Sound3DMinMaxDistance& distance) {
+		ASX_TRY() = handle->set3DMinMaxDistance(distance.min, distance.max);
+	}
+
+	Sound3DMinMaxDistance Sound::get3DMinMaxDistance() const {
+
+		float min;
+		float max;
+
+		ASX_TRY() = handle->get3DMinMaxDistance(&min, &max);
+
+		return { min, max };
+
+	}
+
+	void Sound::set3DConeSettings(const ConeSettings& settings) {
+		ASX_TRY() = handle->set3DConeSettings(settings.insideConeAngle, settings.outsideConeAngle, settings.outsideVolume);
+	}
+
+	ConeSettings Sound::get3DConeSettings() const {
+
+		ConeSettings settings;
+
+		ASX_TRY() = handle->get3DConeSettings(&settings.insideConeAngle, &settings.outsideConeAngle, &settings.outsideVolume);
+
+		return settings;
+
+	}
+
+	void Sound::set3DCustomRolloff(std::span<Vec3f> points) {
+
+		auto list = castVec(points.data());
+		auto size = points.size();
+
+		ASX_TRY() = handle->set3DCustomRolloff(list, size);
+
+	}
+
+	std::span<const Vec3f> Sound::get3DCustomRolloff() const {
+
+		FMOD_VECTOR* points;
+		int numpoints;
+
+		ASX_TRY() = handle->get3DCustomRolloff(&points, &numpoints);
+
+		return { castVec(points), SizeT(numpoints) };
+
+	}
+	
+	void Sound::getSubSound(int index, Sound& sound) {
+
+		FMOD::Sound* fmodSound{};
+
+		ASX_TRY() = handle->getSubSound(index, &fmodSound);
+
+		sound.handle = fmodSound;
+
+	}
+
+	void Sound::getSubSoundParent(Sound& parent) {
+
+		FMOD::Sound* fmodSound{};
+
+		ASX_TRY() = handle->getSubSoundParent(&fmodSound);
+
+		parent.handle = fmodSound;
+
+	}
+
+	std::string Sound::getName() const {
+
+		std::string name;
+		name.resize(1024);
+
+		ASX_TRY() = handle->getName(name.data(), name.size());
+
+		name.shrink_to_fit();
+
+		return name;
+
+	}
+
+	u32 Sound::getLength(TimeUnit unit) const {
+
+		u32 length{};
+
+		ASX_TRY() = handle->getLength(&length, Conv::fromTimeUnit(unit));
+
+		return length;
+
+	}
+
+	SoundFormatInfo Sound::getFormatInfo() const {
+
+		FMOD_SOUND_TYPE type;
+		FMOD_SOUND_FORMAT format;
+		i32 channels, bits;
+
+		ASX_TRY() = handle->getFormat(&type, &format, &channels, &bits);
+
+		return { SoundType(type), SoundFormat(format), u32(channels), u32(bits) };
+
+	}
+
+	u32 Sound::getSubSoundCount() const {
+
+		int count{};
+
+		ASX_TRY() = handle->getNumSubSounds(&count);
+
+		return count;
+
+	}
+
+	// getTagCount
+
+	std::optional<FMOD_TAG> Sound::getTag(const std::string& name, int index) const {
+
+		FMOD_TAG tag{};
+		const char* tagName = name.empty() ? nullptr : name.c_str();
+		int tagIndex = index;
+
+		// TODO: cheat
+		return handle->getTag(tagName, tagIndex, &tag) != FMOD_OK ? std::nullopt : std::optional(tag);
+
+	}
+	
+	SoundOpenStateInfo Sound::getOpenState() const {
+
+		FMOD_OPENSTATE openState;
+		SoundOpenStateInfo info{};
+
+		ASX_TRY() = handle->getOpenState(&openState, &info.percentBuffered, &info.starving, &info.diskBusy);
+
+		info.openState = SoundOpenState(openState);
+
+		return info;
+
+	}
+	
+	void Sound::seekData(u32 offsetPCM) {
+		ASX_TRY() = handle->seekData(offsetPCM);
+	}
+
+	// setSoundGroup
+	// getSoundGroup
+
+	u32 Sound::getSyncPointCount() const {
+
+		int count{};
+
+		ASX_TRY() = handle->getNumSyncPoints(&count);
+
+		return count;
+
+	}
+
+	// getSyncPoint
+	// getSyncPointInfo
+	// addSyncPoint
+	// deleteSyncPoint
+
+	// setMode
+	// getMode
+
+	void Sound::setLoopCount(u32 count) {
+		ASX_TRY() = handle->setLoopCount(count);
+	}
+
+	u32 Sound::getLoopCount() const {
+
+		int count{};
+
+		ASX_TRY() = handle->getLoopCount(&count);
+
+		return count;
+
+	}
+
+	void Sound::setLoopPoints(const TimePoint& start, const TimePoint& end) {
+		ASX_TRY() = handle->setLoopPoints(start.value, start.getUnit(), end.value, end.getUnit());
+	}
+
+	LoopPoints Sound::getLoopPoints(TimeUnit startUnit, TimeUnit endUnit) {
+
+		u32 start{}, end{};
+
+		ASX_TRY() = handle->getLoopPoints(&start, Conv::fromTimeUnit(startUnit), &end, Conv::fromTimeUnit(endUnit));
+
+		return { start, end };
+
+	}
+
+	// getMusicChannelCount
+	// setMusicChannelVolume
+	// getMusicChannelVolume
+	// setMusicSpeed
+	// getMusicSpeed
+
+	void Sound::playImpl(bool paused, FMOD::ChannelGroup* group, FMOD::Channel** channel) {
+
+		if (!handle) {
+			return;
+		}
+
+		if (nonBlocking) 
+			ASX_TRY(FMOD_ERR_NOTREADY) = getSystemHandle()->playSound(handle, group, paused, channel);
+		else 
+			ASX_TRY() = getSystemHandle()->playSound(handle, group, paused, channel);
+
+	}
+
+	void Sound::playImpl(const SoundOptions& opt, bool paused, FMOD::ChannelGroup* group, FMOD::Channel** channel) {
+
+		if (!handle) {
+			return;
+		}
+
+		u32 mode = opt.getFlag();
+		if (mode) {
+			handle->setMode(mode);
+		}
+
+		if (opt.nonBlocking)
+			nonBlocking = true;
+
+		if (nonBlocking)
+			ASX_TRY(FMOD_ERR_NOTREADY) = getSystemHandle()->playSound(handle, group, paused, channel);
+		else
+			ASX_TRY() = getSystemHandle()->playSound(handle, group, paused, channel);
+
+	}
+
+}
