441edfab4889214f0289920d47f775c144c19775
KonPet <45455208+KonPet@users.noreply.github.com>
2022-04-12 19:37:35 +0200

    Hotfix for styling

--- src/arclight/core/image/decode/qoidecoder.cpp
+++ src/arclight/core/image/decode/qoidecoder.cpp
@@ -1,10 +1,25 @@
+/*
+ *	 Copyright (c) 2022 - Arclight Team
+ *
+ *	 This file is part of Arclight. All rights reserved.
+ *
+ *	 qoidecoder.cpp
+ */
+
 #include "qoidecoder.hpp"
 
-constexpr u8 hash(u8 r, u8 g, u8 b, u8 a) {
+
+
+constexpr static u8 hash(u8 r, u8 g, u8 b, u8 a) {
     return (r * 3 + g * 5 + b * 7 + a * 11) % 64;
 }
 
+
+
 void QOIDecoder::decode(std::span<const u8> data) {
+
+	validDecode = false;
+
     reader = BinaryReader(data, ByteOrder::Big);
 
     if (reader.remainingSize() < 14) {
@@ -13,14 +28,12 @@ void QOIDecoder::decode(std::span<const u8> data) {
 
     u32 magic = reader.read<u32>();
 
-    if (magic != 0x716f6966) {
+    if (magic != 0x716F6966) {
         throw ImageDecoderException("QOI magic doesn't match");
     }
 
     u32 width = reader.read<u32>();
     u32 height = reader.read<u32>();
-
-    image = RawImage(width, height);
 
     u8 channels = reader.read<u8>();
     u8 colorspace = reader.read<u8>();
@@ -33,7 +46,9 @@ void QOIDecoder::decode(std::span<const u8> data) {
     Image<Pixel::RGBA8> bufImage(width, height);
 
     for (u32 y = 0; y < height; y++) {
+
         for (u32 x = 0; x < width; x++) {
+
             if (!reader.remainingSize()) {
                 throw ImageDecoderException("QOI stream size too small");
             }
@@ -41,6 +56,7 @@ void QOIDecoder::decode(std::span<const u8> data) {
             u8 tag = reader.read<u8>();
 
             if (tag == 0xff) {
+
                 if (reader.remainingSize() < 4) {
                     throw ImageDecoderException("QOI stream size too small");
                 }
@@ -52,9 +68,10 @@ void QOIDecoder::decode(std::span<const u8> data) {
 
                 bufImage.setPixel(x, y, prevP);
 
-//                palette[hash(prevP.getRed(), prevP.getBlue(), prevP.getGreen(), prevP.getAlpha())] = prevP;
                 palette[hash(prevP.getRed(), prevP.getGreen(), prevP.getBlue(), prevP.getAlpha())] = prevP;
+
             } else if (tag == 0xfe) {
+
                 if (reader.remainingSize() < 3) {
                     throw ImageDecoderException("QOI stream size too small");
                 }
@@ -66,50 +83,84 @@ void QOIDecoder::decode(std::span<const u8> data) {
                 bufImage.setPixel(x, y, prevP);
 
                 palette[hash(prevP.getRed(), prevP.getGreen(), prevP.getBlue(), prevP.getAlpha())] = prevP;
-            } else if (tag >> 6 == 0b00) {
-                bufImage.setPixel(x, y, palette[tag]);
-                prevP = palette[tag];
-            } else if (tag >> 6 == 0b01) {
-                prevP.setRed((prevP.getRed() + ((tag >> 4) & 0x3) - 2) % 256);
-                prevP.setGreen((prevP.getGreen() + ((tag >> 2) & 0x3) - 2) % 256);
-                prevP.setBlue((prevP.getBlue() + (tag & 0x3) - 2) % 256);
 
-                bufImage.setPixel(x, y, prevP);
-                palette[hash(prevP.getRed(), prevP.getGreen(), prevP.getBlue(), prevP.getAlpha())] = prevP;
-            } else if (tag >> 6 == 0b10) {
-                if (!reader.remainingSize()) {
-                    throw ImageDecoderException("QOI stream size too small");
-                }
+            } else {
 
-                u8 dg = (tag & 0x3f) - 32;
+				switch (tag >> 6) {
 
-                prevP.setGreen(prevP.getGreen() + dg);
+					case 0b00:
 
-                u8 dg_db = reader.read<u8>();
+		                bufImage.setPixel(x, y, palette[tag]);
+		                prevP = palette[tag];
 
-                prevP.setRed(prevP.getRed() + dg + (dg_db >> 4) - 8);
-                prevP.setBlue(prevP.getBlue() + dg + (dg_db & 0xf) - 8);
+						break;
 
-                bufImage.setPixel(x, y, prevP);
-                palette[hash(prevP.getRed(), prevP.getGreen(), prevP.getBlue(), prevP.getAlpha())] = prevP;
-            } else if (tag >> 6 == 0b11) {
-                bufImage.setPixel(x, y, prevP);
-                for (u8 rl = 0; rl < (tag & 0x3f); rl++) {
-                    x++;
-                    if (x == width) {
-                        x = 0;
-                        y++;
-                    }
-                    if (y == height) {
-                        throw ImageDecoderException("QOI too many pixels");
-                    }
+					case 0b01:
 
-                    bufImage.setPixel(x, y, prevP);
-                }
-            }
+		                prevP.setRed((prevP.getRed() + ((tag >> 4) & 0x3) - 2) % 256);
+		                prevP.setGreen((prevP.getGreen() + ((tag >> 2) & 0x3) - 2) % 256);
+		                prevP.setBlue((prevP.getBlue() + (tag & 0x3) - 2) % 256);
+
+		                bufImage.setPixel(x, y, prevP);
+		                palette[hash(prevP.getRed(), prevP.getGreen(), prevP.getBlue(), prevP.getAlpha())] = prevP;
+
+						break;
+
+					case 0b10:
+						{
+							if (!reader.remainingSize()) {
+								throw ImageDecoderException("QOI stream size too small");
+							}
+
+							u8 dg = (tag & 0x3f) - 32;
+
+							prevP.setGreen(prevP.getGreen() + dg);
+
+							u8 dg_db = reader.read<u8>();
+
+							prevP.setRed(prevP.getRed() + dg + (dg_db >> 4) - 8);
+							prevP.setBlue(prevP.getBlue() + dg + (dg_db & 0xF) - 8);
+
+							bufImage.setPixel(x, y, prevP);
+							palette[hash(prevP.getRed(), prevP.getGreen(), prevP.getBlue(), prevP.getAlpha())] = prevP;
+						}
+
+						break;
+
+					case 0b11:
+
+		                bufImage.setPixel(x, y, prevP);
+
+		                for (u8 rl = 0; rl < (tag & 0x3F); rl++) {
+
+		                    x++;
+
+		                    if (x == width) {
+
+		                        x = 0;
+		                        y++;
+
+		                    }
+
+		                    if (y == height) {
+		                        throw ImageDecoderException("QOI too many pixels");
+		                    }
+
+		                    bufImage.setPixel(x, y, prevP);
+
+		                }
+
+						break;
+
+				}
+
+			}
+
         }
+
     }
 
     image = bufImage.makeRaw();
     validDecode = true;
+
 }
