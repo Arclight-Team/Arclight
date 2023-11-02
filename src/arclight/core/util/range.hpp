a78e673791acf7049ddf9505e882b3aaaf437764
ItzTacos <47322543+ItzTacosOfficial@users.noreply.github.com>
2023-11-02 19:32:23 +0100

    Added multidimensional Range

--- src/arclight/core/util/range.hpp
+++ src/arclight/core/util/range.hpp
@@ -14,19 +14,22 @@
 #include <iterator>
 
 
-
-template<CC::Arithmetic T, CC::Arithmetic StepT>
+template<SizeT N, CC::Arithmetic T, CC::Arithmetic StepT> requires(N != 0)
 class Range {
 
 public:
+
+	static constexpr SizeT Dimensions = N;
+	static constexpr bool Multidimensional = Dimensions > 1;
+
 
 	class Iterator;
 
 	struct Sentinel {
 
-		constexpr explicit Sentinel(T end) noexcept : end(end) {}
+		constexpr explicit Sentinel(const std::array<T, N>& end) noexcept : end(end) {}
 
-		T end;
+		std::array<T, N> end;
 
 	};
 
@@ -35,58 +38,109 @@ public:
 	public:
 
 		using iterator_category = std::bidirectional_iterator_tag;
-		using value_type = T;
+		using value_type = TT::Conditional<Multidimensional, std::array<T, N>, T>;
 		using pointer = value_type;
 		using reference = value_type;
 		using element_type = value_type;
 
-		constexpr explicit Iterator() noexcept : x(0), step(0) {}
-		constexpr explicit Iterator(T start, StepT step) noexcept : x(start), step(step) {}
+		constexpr explicit Iterator() noexcept : cur(), step(0) {}
+		constexpr explicit Iterator(const std::array<T, N>& start, StepT step) noexcept : start(start), cur(start), step(step) {}
 
-		constexpr reference operator*() const noexcept { return x; }
-		constexpr pointer operator->() const noexcept { return x; }
+		constexpr reference operator*() const noexcept {
+
+			if constexpr (Multidimensional) {
+				return cur;
+			} else {
+				return cur[0];
+			}
+
+		}
+
+		constexpr pointer operator->() const noexcept {
+			return operator*();
+		}
 
 		constexpr Iterator& operator++() noexcept {
-			x += step;
+
+			cur[0] += step;
+
 			return *this;
+
 		}
 
 		constexpr Iterator& operator--() noexcept {
-			x -= step;
+
+			cur[0] -= step;
+
 			return *this;
+
 		}
 
 		constexpr Iterator operator++(int) noexcept {
-			auto cpy = *this;
+
+			auto copy = *this;
+
 			++(*this);
-			return cpy;
+
+			return copy;
+
 		}
 
 		constexpr Iterator operator--(int) noexcept {
-			auto cpy = *this;
+
+			auto copy = *this;
+
 			--(*this);
-			return cpy;
+
+			return copy;
+
 		}
 
 		constexpr bool operator==(const Iterator& other) const noexcept {
-			return x == other.x;
+
+			for (SizeT i = 0; i < Dimensions; i++) {
+
+				if (cur[i] != other.x[i]) {
+					return false;
+				}
+
+			}
+
+			return true;
+
 		}
 
 		constexpr auto operator<=>(const Iterator& other) const noexcept {
-			return x <=> other.x;
+			return cur <=> other.cur;
 		}
 
-		constexpr bool operator==(Sentinel s) noexcept {
-			return x >= s.end;
+		constexpr bool operator==(Sentinel sentinel) noexcept {
+
+			constexpr SizeT Count = Dimensions - 1;
+
+			for (SizeT i = 0; i < Count; i++) {
+
+				if (cur[i] >= sentinel.end[i]) {
+
+					cur[i] = start[i];
+					cur[i + 1] += step;
+
+				}
+
+			}
+
+			return cur[Count] >= sentinel.end[Count];
+
 		}
 
-		friend constexpr bool operator==(Sentinel s, const Iterator& iter) noexcept {
-			return iter == s;
+		friend constexpr bool operator==(Sentinel sentinel, const Iterator& iter) noexcept {
+			return iter == sentinel;
 		}
 
 	private:
 
-		T x;
+		std::array<T, N> start;
+		std::array<T, N> cur;
 		StepT step;
 
 	};
@@ -95,11 +149,13 @@ public:
 	using ReverseIterator = std::reverse_iterator<Iterator>;
 
 
+	constexpr Range(const std::array<T, N>& start, const std::array<T, N>& stop, StepT step = 1) noexcept requires(Multidimensional) : start(start), stop(stop), step(step) {}
 
-	constexpr explicit Range(T stop) noexcept : Range(0, stop, 1) {}
+	constexpr explicit Range(const std::array<T, N>& stop) noexcept requires(Multidimensional) : Range({}, stop, 1) {}
 
-	template<CC::Arithmetic T0, CC::Arithmetic T1, CC::Arithmetic T2, CC::Arithmetic U = TT::CommonType<T0, T1>>
-	constexpr Range(T0 start, T1 stop, T2 step = 1) noexcept : start(start), stop(stop), step(step) {}
+	constexpr Range(T start, T stop, StepT step = 1) noexcept requires(!Multidimensional) : start{start}, stop{stop}, step(step) {}
+
+	constexpr explicit Range(T stop) noexcept requires(!Multidimensional) : Range(0, stop, 1) {}
 
 
 	constexpr Iterator begin() const noexcept {
@@ -136,16 +192,28 @@ public:
 
 private:
 
-	T start;
-	T stop;
+	std::array<T, N> start;
+	std::array<T, N> stop;
 	StepT step;
 
 };
 
 
 
+template<SizeT N, CC::Arithmetic T> requires(N != 0)
+Range(const T (&)[N]) -> Range<N, T, T>;
+
+template<SizeT N, CC::Arithmetic T0, CC::Arithmetic T1, CC::Arithmetic T2> requires(N != 0)
+Range(const T0 (&)[N], const T1 (&)[N], T2) -> Range<N, TT::CommonType<T0, T1>, T2>;
+
+template<SizeT N, CC::Arithmetic T> requires(N != 0)
+Range(const std::array<T, N>&) -> Range<N, T, T>;
+
+template<SizeT N, CC::Arithmetic T0, CC::Arithmetic T1, CC::Arithmetic T2> requires(N != 0)
+Range(const std::array<T0, N>&, const std::array<T1, N>&, T2) -> Range<N, TT::CommonType<T0, T1>, T2>;
+
 template<CC::Arithmetic T>
-Range(T) -> Range<T, T>;
+Range(T) -> Range<1, T, T>;
 
 template<CC::Arithmetic T0, CC::Arithmetic T1, CC::Arithmetic T2>
-Range(T0, T1, T2) -> Range<TT::CommonType<T0, T1>, T2>;
+Range(T0, T1, T2) -> Range<1, TT::CommonType<T0, T1>, T2>;
