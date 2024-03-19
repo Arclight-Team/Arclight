6391a78f67f325831e7d71dccae45b3994d9f180
ItzTacos <47322543+ItzTacosOfficial@users.noreply.github.com>
2024-03-19 23:16:08 +0100

    Updated assert mechanism
    Added compile time assertion support

--- src/arclight/core/util/assert.hpp
+++ src/arclight/core/util/assert.hpp
@@ -9,8 +9,8 @@
 #pragma once
 
 #include "log.hpp"
-#include "common/types.hpp"
 #include "common/config.hpp"
+#include "common/intrinsic.hpp"
 
 #include <utility>
 #include <source_location>
@@ -21,47 +21,61 @@
 #define arc_exit(code)	do { std::exit((code)); } while (false)
 
 
-template<class... Args>
-constexpr void __arc_assert(bool condition, const std::source_location& location, const std::string& message, Args&&... args) noexcept {
+#ifdef ARC_CFG_ASSERT_VERBOSE
+	#define __ARC_ASSERT_CONSTEXPR_FAIL __It_could_be_that_perchance_the_Arclight_assertion_mechanism_had_a_negative_outcome_during_compile_time_evaluation
+#else
+	#define __ARC_ASSERT_CONSTEXPR_FAIL __arc_assert_failed_at_compile_time
+#endif
 
-	if (condition) {
+static constexpr void __arc_assert_constexpr(bool condition) noexcept {
 
-		return;
+	void __ARC_ASSERT_CONSTEXPR_FAIL();
 
-	} else if (std::is_constant_evaluated()) {
-
-		std::unreachable(); // Simplify constant evaluation failure
-
-	} else {
-
-		try {
-
-			LogE("Assert").print("Assertion failed in file '%s' [%d:%d] in function '%s':", location.file_name(), location.line(), location.column(), location.function_name());
-			LogE("Assert").print("\t" + message, std::forward<Args>(args)...);
-
-		} catch (...) {
-
-			LogE("Assert") << "Fatal error: Failed to assert";
-
-		}
-
-		arc_abort();
-
+	if (std::is_constant_evaluated() && !condition) {
+		__ARC_ASSERT_CONSTEXPR_FAIL();
 	}
 
 }
 
+#undef __ARC_ASSERT_CONSTEXPR_FAIL
 
-// TODO [[assume(cond)]] in release
 
-#if defined(ARC_FINAL_BUILD) || (defined(ARC_ASSERT_DEBUG) && ARC_RELEASE) || defined(ARC_ASSERT_NEVER)
+template<class... Args>
+static constexpr void __arc_assert(bool condition, const std::source_location& location, const std::string& message, Args&&... args) noexcept {
 
-	#define arc_assert(cond, msg, ...) do {} while (false)
-	#define arc_force_assert(msg, ...) do {} while (false)
+	if (condition) {
+		return;
+	}
 
-#else
+	try {
 
-	#define arc_assert(cond, msg, ...) do { __arc_assert(!!(cond), std::source_location::current(), msg __VA_OPT__(,) __VA_ARGS__); } while (false)
-	#define arc_force_assert(msg, ...) arc_assert(false, msg __VA_OPT__(,) __VA_ARGS__)
+		LogE("Assert").print("Assertion failed in file '%s' [%d:%d] in function '%s':", location.file_name(), location.line(), location.column(), location.function_name());
+		LogE("Assert").print("\t" + message, std::forward<Args>(args)...);
 
+	} catch (...) {
+
+		LogE("Assert") << "Fatal error: Failed to assert";
+
+	}
+
+	arc_abort();
+
+}
+
+
+#if (ARC_CFG_ASSERT_POLICY == ARC_ASSERT_POLICY_DEBUG && ARC_DEBUG) || (ARC_CFG_ASSERT_POLICY == ARC_ASSERT_POLICY_BUILD && !defined(ARC_CFG_FINAL_BUILD))
+	#define ARC_ASSERT_ENABLED
 #endif
+
+
+#ifdef ARC_ASSERT_ENABLED
+	#define __ARC_ASSERT_IMPL(cond, msg, ...) __arc_assert(!!(cond), std::source_location::current(), msg __VA_OPT__(,) __VA_ARGS__);
+#elif defined(ARC_CFG_ASSERT_ASSUME)
+	#define __ARC_ASSERT_IMPL(cond, msg, ...) arc_assume(cond);
+#else
+	#define __ARC_ASSERT_IMPL(cond, msg, ...)
+#endif
+
+#define arc_assert(cond, msg, ...) do { __arc_assert_constexpr(cond); __ARC_ASSERT_IMPL(cond, msg __VA_OPT__(,) __VA_ARGS__) } while (false)
+
+#define arc_force_assert(msg, ...) arc_assert(false, msg __VA_OPT__(,) __VA_ARGS__)
